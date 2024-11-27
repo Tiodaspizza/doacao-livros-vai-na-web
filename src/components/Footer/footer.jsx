b48a7982ebcef54aa764676e8e20b362c8d913e2
Matheus Novaes <149258757+Tiodaspizza@users.noreply.github.com>
2024-11-27 15:02:51 -0300

    Corrigindo importações e ajustes no projeto

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.scss';
 import facebook from "../../assets/facebook.png";
 import instagram from "../../assets/instagram.png";
 import linkedin from "../../assets/linkedin.png";
-import twitter from "../../assets/twitter.png";
+const twitter = require('../../assets/twitter.png');
 import youtube from "../../assets/youtube.png";
 
 
