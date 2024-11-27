c0f96b525bf8750742043b0e510afdac9f1e41a0
Matheus Novaes <149258757+Tiodaspizza@users.noreply.github.com>
2024-11-27 15:24:53 -0300

    Corrige caminho de importação da imagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header/header';
 import './sass/global.scss';
-import Footer from './components/Footer/footer'
+import Footer from './components/Footer/Footer'
 
 
 export default function App(){
