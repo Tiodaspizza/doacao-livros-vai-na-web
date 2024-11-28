9c92d5b3ccc865f3778d051c7cf8341b16675ba7
Matheus Novaes <149258757+Tiodaspizza@users.noreply.github.com>
2024-11-28 11:54:32 -0300

    adicionando imagem twitter

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,7 +2,7 @@ import styles from './Footer.module.scss';
 import facebook from "../../assets/facebook.png";
 import instagram from "../../assets/instagram.png";
 import linkedin from "../../assets/linkedin.png";
-import twittgit er from "../../assets/twitter.png"
+import twitter from "../../assets/twitter.png"
 import youtube from "../../assets/youtube.png";
 
 
