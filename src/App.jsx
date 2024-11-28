0f6e577fa6b67fb1ddd38741c20ffb968b111bfe
Matheus Novaes <149258757+Tiodaspizza@users.noreply.github.com>
2024-11-28 11:32:42 -0300

    Corrigindo importações de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import Footer from './components/Footer/footer';
-import Header from './components/Header/Header';
+import Header from './components/Header/Header.jsx';
 
 
 import './sass/global.scss';
-
-
-
 
 export default function App(){
   return(
